9683d298085eb857701e525d41de99e4b358be64
Robel-worku <107203052+Robel-worku@users.noreply.github.com>
2025-03-26 15:18:22 +0300

    Create swapcase,findandindex.py

---
+++ Robel_Worku/string_method/swapcase, find and index/swapcase,findandindex.py
@@ -0,0 +1,12 @@
+txt = "this is a test sentence"
+
+print(txt.swapcase())           # swapcase
+
+print(txt.find("a"))            # find, has found the value at 8
+print(txt.find("w"))            # couldn't find "w" , so returned -1
+
+
+
+
+print(txt.index("a"))           # index, has found the value at 8
+print(txt.index("w"))            # couldn't find "w" , so returned an error message
