542935a90247e954becfded07379d4eb64117b40
Robel-worku <107203052+Robel-worku@users.noreply.github.com>
2025-03-26 15:06:25 +0300

    Create upper,titleandcapitalize.py

---
+++ Robel_Worku/string_method/upper, title and capitalize/upper,titleandcapitalize.py
@@ -0,0 +1,13 @@
+txt = "this is a test text"
+
+print(txt.upper())                     # upper
+
+
+sentence = "there is a car"
+
+print(sentence.title())                # title
+
+
+my_text = "there is a ball"
+
+print(my_text.capitalize())         # capitalize
